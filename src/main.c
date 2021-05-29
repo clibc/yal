58a6ee6fb43a8fea5873397481924398fccbccb6
clibc <shirvani.abddullayev@gmail.com>
2021-05-29 23:58:40 +0200

    unix windows seperator macro

--- src/main.c
+++ src/main.c
@@ -4,13 +4,25 @@
 #include "lexer.h"
 #include "parser.h"
 
+#ifdef _WIN32
+#define GET_LINE												\
+	char line[100];												\
+	size_t line_size = sizeof(line);							\
+	fgets(line, 100, stdin)									
+#elif __unix__
+#define GET_LINE												\
+	char *line = NULL;											\
+	size_t line_size;											\
+	getline(&line, &line_size, stdin);							
+#endif
+
+
 int main(void) {
 	/// 86 + 78 - 36
-	char* str = NULL;
-	size_t line_size;
-	getline(&str, &line_size, stdin);
 
-	lex_line(str, line_size);
+	GET_LINE;
+	
+	lex_line(line, line_size);
 
 	dump_tokens();
 
